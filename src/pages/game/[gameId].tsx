f8e985cab68222cfadb1585fd9e3bf0048f433cd
Benjamin Knox <bknox.contact@gmail.com>
2023-12-21 21:26:48 -0500

    [Board]: Show latest game

--- src/pages/game/[gameId].tsx
+++ src/pages/game/[gameId].tsx
@@ -1,10 +1,14 @@
 import { Chess } from "chess.js";
-import { useEffect } from 'react';
-import { Server } from 'Socket.IO'
-import io from 'Socket.IO-client';
+import { Document } from "mongoose";
+import { useEffect } from "react";
+import { Server } from "Socket.IO";
+import io from "Socket.IO-client";
+import { Game as GameType } from "../../types";
 import { GameModel } from "../../entities";
+import { getServerSession } from "next-auth/next";
 import type { Chess as ChessType } from "chess.js";
 import type { GetServerSidePropsContext } from "next";
+import { authOptions } from "../api/auth/[...nextauth]";
 import { mongoConnect } from "../../middleware/database";
 import { BoardContainer } from "../../components/BoardContainer";
 
@@ -19,22 +23,39 @@ export default function Game({
 }
 
 export const getServerSideProps = async (
-  context: GetServerSidePropsContext & { res :{ socket: any } }
+  context: GetServerSidePropsContext & { res: { socket: any } }
 ) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
   if (!context.res.socket.server.io) {
     context.res.socket.server.io = new Server(context.res.socket.server, {
-      path: "/coms"
-    })
+      path: "/coms",
+    });
   }
 
   await mongoConnect();
 
-  const game = await GameModel.findOne({ id: context.query.gameId }).exec();
+  let game: GameType<string>;
+
+  if (context.query.gameId === "latest") {
+    const userId = (session.user as { id: string }).id;
+    game = await GameModel.findOne<GameType<string>>({
+      $or: [{ white_player: userId }, { black_player: userId }],
+    })
+      .sort({ createdAt: -1 })
+      .exec();
+  } else {
+    game = await GameModel.findOne<GameType<string>>({
+      id: context.query.gameId,
+    }).exec();
+  }
+
+  const lastMove = game.moves[game.moves.length - 1].move;
 
   return {
-    props: { 
-      gameId: context.query.gameId,
-      lastMove: game.moves[game.moves.length - 1].move
+    props: {
+      gameId: game.id,
+      lastMove,
     },
   };
 };
